fc80d273ce43798c94edbda229baa03d2909db17
EscobarLuis <escobarluis77@gmail.com>
2023-04-26 10:26:46 -0300

    Segundo ejercicio

---
+++ Ejer2/index.js
@@ -0,0 +1,13 @@
+
+const fechaNac = new Date(1983, 10, 21);
+
+const libro = {
+    titulo: "Harry Potter y la piedra filosofal",
+    autor: "J. K. Rowling",
+    fecha: new Date("June 26 1997"),
+    url: "https://www.amazon.es/Harry-Potter-Piedra-Filosofal-Rowling/dp/8478884459",
+}
+
+const datos = ["Luis", 39, false, fechaNac, libro];
+
+console.log(datos);
