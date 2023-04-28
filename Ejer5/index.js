db7b6cf322a2da6994d498494c71c2aed8d8befd
EscobarLuis <escobarluis77@gmail.com>
2023-04-28 12:06:15 -0300

    Agrego ejercicio 5

---
+++ Ejer5/index.js
@@ -0,0 +1,15 @@
+let alturaCM = 180;
+let alturaM = 1.8;
+
+let pesoKG =75.5;
+
+let alturaRedUp = Math.round(alturaM); 
+let pesoRedDown = Math.floor(pesoKG); 
+
+console.log(`${alturaRedUp}  ${pesoRedDown}`);
+
+let maximoValor = Number.MAX_VALUE; 
+let esIgual = (maximoValor + 1) === maximoValor;
+
+console.log(esIgual);
+
